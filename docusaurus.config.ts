0747dbea75c01c3c75dc77968a404f33d271e51f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-06-02 18:18:26 -0700

    Fix the home page routing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,9 @@ const config: Config = {
           // sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/not-slop/website/blob/main/'
+          editUrl: 'https://github.com/not-slop/website/blob/main/',
+
+          routeBasePath: '/'
         },
         theme: {
           customCss: './src/css/custom.css'
