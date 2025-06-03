ba74cc9cb65b8e1959fb9d90c965e532c4d155b3
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-06-02 19:47:34 -0700

    Add ograph image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/not-slop-ograph.png',
     colorMode: {
       disableSwitch: true
     },
